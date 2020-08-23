7dff6a57db361631eab206e9e98e432f30241428
robertvitoriano <robertvitorianodev@gmail.com>
2020-08-23 20:17:27 -0300

    bug fix: improving sig up route

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -5,6 +5,7 @@ module.exports = {
     async store(req, res) {
         const users =  await User.find();
         const emailExists =  users.filter(user=>{user.email===req.body.email});
+        console.log(emailExists);
         if (!emailExists){
             try {
                 const user = new User(req.body);
