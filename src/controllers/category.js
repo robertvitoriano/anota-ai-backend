e375f6bdc48899f9d16159889c9cb4b7bf845445
robertvitoriano <robertvitorianodev@gmail.com>
2020-11-03 07:39:02 -0300

    add find all notes

--- src/controllers/category.js
+++ src/controllers/category.js
@@ -1,80 +1,66 @@
-const CategoryModel = require('./../models/Category');
-const UserModel = require('./../models/User');
-const NoteModel = require('./../models/Note')
+const CategoryModel = require("./../models/Category");
+const UserModel = require("./../models/User");
+const NoteModel = require("./../models/Note");
 
 module.exports = {
-    async store(req,res){
-        const user = req.user;
-        const {name} = req.body;
-        const category = new CategoryModel({name:name,authorId:user._id});
+  async store(req, res) {
+    const user = req.user;
+    const { name } = req.body;
+    const category = new CategoryModel({ name: name, authorId: user._id });
 
-        try {
-            await category.save();
-            const loggedUser = await UserModel.findById(user._id);
-            await loggedUser.categoriesId.push(category._id);
-            await loggedUser.save();
+    try {
+      await category.save();
+      const loggedUser = await UserModel.findById(user._id);
+      await loggedUser.categoriesId.push(category._id);
+      await loggedUser.save();
 
-            res.status(201).send(category);
-            
-        } catch (e) {
+      res.status(201).send(category);
+    } catch (e) {
+      console.log(e);
+      res.status(400).send(e);
+    }
+  },
 
-           console.log(e);
-            res.status(400).send(e);
-            
-        }
+  async index(req, res) {
+    try {
+      const categoryId = req.params.categoryId;
+      const category = await CategoryModel.findById(categoryId);
+      const notes = await NoteModel.find({ categoryId: categoryId });
+      console.log(notes);
+      res.send({ category: category, notes: notes });
+    } catch (error) {
+      res.status(400).send(error);
+    }
+  },
+  async list(req, res) {
+    const { user } = req;
+    try {
+      const loggedUser = await UserModel.findById(user._id);
+      const categories = await CategoryModel.find({ authorId: loggedUser._id });
+      res.send(categories);
+    } catch (e) {
+      res.send(e);
+    }
+  },
+  async associate(req, res) {
+    try {
+      const { notesId } = req.body;
+      const { categoryId } = req.params;
+      
 
+      const findAllNotes = async (notesId) => {
 
+        return notesId.map((id) => {
+          const note =  NoteModel.findByIdAndUpdate(id);
+          return note;
+        });
+      };
 
-    },
+    const notes =   await Promise.all(findAllNotes(notesId));
 
-   async index(req,res){
-       try {
-           const categoryId = req.params.categoryId;
-           const category = await CategoryModel.findById(categoryId);
-           const notes = await NoteModel.find({categoryId:categoryId});
-           console.log(notes);
-           res.send({category:category,notes:notes});
-           
-       } catch (error) {
-           res.status(400).send(error);
-           
-       }
+      console.log(notes);
 
-
-   },
-   async list(req,res){
-       const {user} = req;
-       try {
-           const loggedUser = await UserModel.findById(user._id);
-           const categories = await CategoryModel.find({ authorId: loggedUser._id })
-           res.send(categories);
-
-           
-       } catch (e){
-           res.send(e);
-           
-           
-       }
-   },
-   async associate(req,res){
-       try {
-           const {notesId} = req.body;
-           const {categoryId} = req.params;
-           let notes = []
-           notesId.map(async(id)=>{
-            const note =  await NoteModel.findByIdAndUpdate(id);
-            await note.save();
-            notes.push(note)
-           })
-           console.log(notes);
- 
-           res.send(notes);
-
-           
-       } catch  {
-           
-       }
-   },
-
-
-}
+      res.send(notes);
+    } catch {}
+  },
+};
