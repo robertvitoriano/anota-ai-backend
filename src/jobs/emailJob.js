5949cbb46be2221893af2fcbce09426a66660af0
robertvitoriano <robertvitorianodev@gmail.com>
2021-04-08 22:36:33 -0300

    add email cronjob

---
+++ src/jobs/emailJob.js
@@ -0,0 +1,42 @@
+const cron = require('cron')
+const mailer = require('nodemailer')
+const User = require('./../models/User')
+const emailJob = cron.CronJob('*/5 * * * * *', async () => {
+
+
+    const users = await User.find();
+    
+    for (const user of users) {
+
+        if (user.receivedEmail === false) {
+
+
+            const transporter = mailer.findcreateTransport({
+                service: 'gmail',
+                auth: {
+                    user: process.env.EMAIL,
+                    pass: process.env.EMAIL_PASSWORD
+                }
+            });
+
+            const mailOptions = {
+                from: process.env.EMAIL,
+                to: user.email,
+                subject: 'Confirme seu Email',
+                text: 'Código de verificação'
+            };
+
+            transporter.sendMail(mailOptions, async  (error, info) =>{
+                if (error) {
+                    console.log(error);
+                } else {
+                    console.log('Email sent: ' + info.response);
+                    user.receivedEmail = true
+                   await user.save()
+                }
+
+            })
+        }
+    }
+})
+export { emailJob }
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 express = require('express');
 const cors = require('cors');
 const router = require('./routes/router.js');
+const { emailJob } = require('./jobs/emailJob')
 require('./database/connection');
 app = express();
 const PORT  = process.env.PORT
@@ -8,6 +9,9 @@ const PORT  = process.env.PORT
 app.use(cors());
 app.use(express.json());
 app.use(router);
+
+emailJob.start()
+
 
 
 
