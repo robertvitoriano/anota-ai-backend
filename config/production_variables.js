2ffbf3e77c8eff0c039ce4f39d10f3e932a4d980
Robert Vitoriano <robertvitorianodev@gmail.com>
2021-05-21 20:56:12 -0300

    refactor code

--- config/production_variables.js
+++ config/production_variables.js
@@ -1,7 +1,7 @@
 const variables = {
 
   PORT:process.env.PORT,
-  Mongo_URL:process.env.MONGO_URL,
+  MONGO_URL:process.env.MONGO_URL,
   EMAIL_PASSWORD:process.env.EMAIL_PASSWORD,
   EMAIL:process.env.EMAIL,
   SECRET_KEY: process.env.SECRET_KEY
