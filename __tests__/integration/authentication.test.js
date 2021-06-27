f9b258eae409ad1073bd2ab2e644ecad2dbf4232
Robert Vitoriano <robertvitorianodev@gmail.com>
2021-06-27 19:54:23 -0300

    pass login test

--- __tests__/integration/authentication.test.js
+++ __tests__/integration/authentication.test.js
@@ -1,3 +1,7 @@
+
+
+
+
 const  truncate  =  require('../utils/truncate')
 
 const {User} = require('./../../src/models')
@@ -6,6 +10,7 @@ const app = require('./../../src/app')
 
 const request = require('supertest')
 
+jest.setTimeout(30000);
 
 describe('Authentication', () => {
   beforeEach(async () => {
@@ -22,9 +27,20 @@ describe('Authentication', () => {
 
        expect(response.status).toBe(500)
 
-
-
   });
 
-});
 
+  it('should not be able to authenticate after email is confirmed', async()=>{
+    const user  = await User.create({name:'Robert da Silva Vitoriano', password:'123', email:'robertvitoriano@gmail.com', username:'robertvitoriano'})
+    user.confirmed = true
+    user.receivedEmail = true
+    await user.save()
+    const response = await request(app)
+      .post('/users/login')
+      .send({email:user.email, password:'123'})
+
+      expect(response.status).toBe(200)
+ });
+ 
+
+});
