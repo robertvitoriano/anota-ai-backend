bf4f3453656a965d154adb4227001025ac45efae
Robert Vitoriano <robertvitorianodev@gmail.com>
2021-06-27 18:35:26 -0300

    remove console.log in test suite

--- __tests__/integration/authentication.test.js
+++ __tests__/integration/authentication.test.js
@@ -15,8 +15,6 @@ describe('Authentication', () => {
   it('should not be able to authenticate without email confirmation', async()=>{
      const user  = await User.create({name:'Robert da Silva Vitoriano', password:'123', email:'robertvitoriano@gmail.com', username:'robertvitoriano'})
 
-     console.log('USER ', user)
-
      const response = await request(app)
        .post('/users/login')
        .send({email:user.email, password:'123'})
