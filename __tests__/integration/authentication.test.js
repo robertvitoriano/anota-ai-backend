da8cb25c569f31d76ceb741860b868ba2e04b77b
Robert Vitoriano <robertvitorianodev@gmail.com>
2021-06-28 05:44:36 -0300

    add authentication failing test

--- __tests__/integration/authentication.test.js
+++ __tests__/integration/authentication.test.js
@@ -2,9 +2,9 @@
 
 
 
-const  truncate  =  require('../utils/truncate')
+const truncate = require('../utils/truncate')
 
-const {User} = require('./../../src/models')
+const { User } = require('./../../src/models')
 
 const app = require('./../../src/app')
 
@@ -14,33 +14,50 @@ jest.setTimeout(30000);
 
 describe('Authentication', () => {
   beforeEach(async () => {
-      await truncate()
+    await truncate()
   });
 
-  it('should not be able to authenticate without email confirmation', async()=>{
-     const user  = await User.create({name:'Robert da Silva Vitoriano', password:'123', email:'robertvitoriano@gmail.com', username:'robertvitoriano'})
+  it('should not be able to authenticate without email confirmation', async () => {
+    const user = await User.create({ name: 'Robert da Silva Vitoriano', password: '123', email: 'robertvitoriano@gmail.com', username: 'robertvitoriano' })
 
-     const response = await request(app)
-       .post('/users/login')
-       .send({email:user.email, password:'123'})
+    const response = await request(app)
+      .post('/users/login')
+      .send({ email: user.email, password: '123' })
 
 
-       expect(response.status).toBe(500)
+    expect(response.status).toBe(500)
 
   });
 
 
-  it('should be able to authenticate after email is confirmed', async()=>{
-    const user  = await User.create({name:'Robert da Silva Vitoriano', password:'123', email:'robertvitoriano@gmail.com', username:'robertvitoriano'})
+  it('should not be able to authenticate with invalid credentials', async () => {
+    const user = await User.create({ name: 'Robert da Silva Vitoriano', password: '123', email: 'robertvitoriano@gmail.com', username: 'robertvitoriano' })
+
+    user.confirmed = true
+    user.receivedEmail = true
+    await user.save()
+
+    const response = await request(app)
+      .post('/users/login')
+      .send({ email: user.email, password: '1234' })
+
+
+    expect(response.status).toBe(400)
+
+  });
+
+
+  it('should be able to authenticate after email is confirmed', async () => {
+    const user = await User.create({ name: 'Robert da Silva Vitoriano', password: '123', email: 'robertvitoriano@gmail.com', username: 'robertvitoriano' })
     user.confirmed = true
     user.receivedEmail = true
     await user.save()
     const response = await request(app)
       .post('/users/login')
-      .send({email:user.email, password:'123'})
+      .send({ email: user.email, password: '123' })
 
-      expect(response.status).toBe(200)
- });
- 
+    expect(response.status).toBe(200)
+  });
+
 
 });
