5456c66d0962cb9250a5f3a7848fe58831bfa966
Robert Vitoriano <robertvitorianodev@gmail.com>
2021-06-28 05:20:14 -0300

    fix encrypt test

--- __tests__/unit/user.test.js
+++ __tests__/unit/user.test.js
@@ -5,6 +5,7 @@
 const truncate = require('../utils/truncate')
 
 const { User } = require('./../../src/models')
+const bcrypt = require('bcryptjs')
 
 const app = require('./../../src/app')
 
@@ -17,30 +18,12 @@ describe('user', () => {
     await truncate()
   });
 
-  it('should  be able to create user', async () => {
+  it('should  encrypt user password', async () => {
     const user = await User.create({ name: 'Robert da Silva Vitoriano', password: '123', email: 'robertvitoriano@gmail.com', username: 'robertvitoriano' })
 
-    Object.assign(user, {
-      created_at: new Date(),
-      updated_at: new Date(),
-    });
+    const isHashValid = await bcrypt.compare('123', user.password)
 
-    expect(user).toMatchObject({
-        
-      confirmed: false,
-      receivedEmail: false,
-      emailAttempts: 0,
-      notesId: [],
-      categoriesId: [],
-      name: 'Robert da Silva Vitoriano',
-      email: 'robertvitoriano@gmail.com',
-      username: 'robertvitoriano',
-      });
-
-      expect(user.created_at).toBeInstanceOf(Date)
-
-      expect(user.updated_at).toBeInstanceOf(Date)
-
+    expect(isHashValid).toBe(true)
 });
 
 
