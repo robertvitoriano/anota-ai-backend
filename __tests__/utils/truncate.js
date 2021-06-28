28f98cc843edfe12a8bfed96cdbfb27c03e66bf2
Robert Vitoriano <robertvitorianodev@gmail.com>
2021-06-27 22:24:27 -0300

    fix truncate function

--- __tests__/utils/truncate.js
+++ __tests__/utils/truncate.js
@@ -5,10 +5,10 @@ module.exports = async()=>{
 
   for ( let model of Object.keys(models) ) {
     try {
-      await models[String(model)].collection.drop();
+      await models[model].collection.drop();
     } catch (e) {
       if (e.code === 26) {
-        console.log('namespace %s not found',models[String(model)].collection.name)
+        console.log('namespace %s not found',models[model].collection.name)
       } else {
         throw e;
       }
