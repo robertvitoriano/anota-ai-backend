c467c3ffa87c5bf5fd4a507c2d28732f2e15d102
Robert Vitoriano <robertvitoriano@gmail.com>
2021-10-12 19:04:58 -0300

    fix EMAIL_SERVICE config var

--- config/production_variables.js
+++ config/production_variables.js
@@ -4,6 +4,7 @@ const variables = {
   MONGO_URL:process.env.MONGO_URL,
   EMAIL_PASSWORD:process.env.EMAIL_PASSWORD,
   EMAIL:process.env.EMAIL,
+  EMAIL_SERVICE:process.env.EMAIL_SERVICE,
   SECRET_KEY: process.env.SECRET_KEY,
   API_URL:process.env.API_URL
 
