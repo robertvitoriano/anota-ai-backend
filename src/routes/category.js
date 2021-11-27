2d0595ae341d7edf78e3926849252997d1923b52
Robert Vitoriano <robertvitoriano@gmail.com>
2021-11-27 08:41:39 -0300

    remove unnecessary paratemetes in categories routes

--- src/routes/category.js
+++ src/routes/category.js
@@ -2,14 +2,11 @@ const CategoryRouter = require('express').Router();
 const CategoryController = require('./../controllers/category')
 const auth = require('./../middleware/auth')
 
-
-
-CategoryRouter.post('/:userId/categories',auth,CategoryController.store);
-CategoryRouter.get('/:userId/categories',auth, CategoryController.list);
-CategoryRouter.get('/:userId/categories/:categoryId',auth, CategoryController.index);
+CategoryRouter.post('/categories',auth,CategoryController.store);
+CategoryRouter.get('/categories',auth, CategoryController.list);
+CategoryRouter.get('/categories/:categoryId',auth, CategoryController.index);
 CategoryRouter.post('/categories/:categoryId/associate',auth,CategoryController.associate);
 CategoryRouter.patch('/categories/:categoryId/remove',auth,CategoryController.remove);
-
 
 module.exports = CategoryRouter;
 
