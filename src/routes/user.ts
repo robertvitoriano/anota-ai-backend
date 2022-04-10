1e258f25af3c03e2ea9bfb7bcf96e2b2b67165c7
Robert Vitoriano <robertvitoriano@gmail.com>
2022-04-10 09:42:19 -0300

    ANOTAB-3  use createUserUseCase in router

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,11 +1,13 @@
 import { Router, Response, Request } from 'express'
 import { loginController } from './../domain/user/useCases/LoginUseCase';
+import { createUserController } from './../domain/user/useCases/CreateUserUseCase'
 import userController from './../controllers/user'
 import auth from '../middleware/auth'
 
 const userRouter = Router()
 //Sign LOG IN
-userRouter.post('/users', userController.store);
+userRouter.post('/users', (request: Request, response: Response) =>
+  createUserController.handle(request, response));
 userRouter.post('/users/login', (request: Request, response: Response) =>
   loginController.handle(request, response));
 userRouter.get('/users/me', auth, userController.index);
